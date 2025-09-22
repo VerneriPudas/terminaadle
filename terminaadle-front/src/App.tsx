6cf7a731dc5c6125710f8035a82022c6bdd69bdc
Santeri Hyvärinen <santeri.hyvarinen@icloud.com>
2025-09-23 00:05:09 +0300

    Improve visuals of first draft  and start working on answer handling

--- terminaadle-front/src/App.tsx
+++ terminaadle-front/src/App.tsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
 import "./styles/App.css";
+import "./styles/guesses.css";
+
+interface GuessProps {
+  issue: number | null;
+  releaseYear: number | null;
+  hint?: string;
+}
+
+function Guess({ issue, releaseYear, hint }: GuessProps) {
+  return (
+    <div>
+      {issue} - {releaseYear}
+      <span>{hint}</span>
+    </div>
+  );
+}
 
 function App() {
+  const [guesses, setGuesses] = useState<GuessProps[]>([]);
+
   function onFormSubmit(formData: FormData): void {
     console.log("Submited", formData);
+    console.log(guesses);
+
+    const guessObject: GuessProps = {
+      issue: formData.get("issue") as unknown as number,
+      releaseYear: formData.get("release_year") as unknown as number,
+    };
+    setGuesses((prev) => [...prev, guessObject]);
   }
 
   return (
@@ -11,6 +36,19 @@ function App() {
       <h1>Terminaadle</h1>
       <div id="img-container">
         <div id="terminaali-page"></div>
+        <div id="guesses">
+          <div className="guesses-content">
+            <h3>Arvaukset</h3>
+            {guesses.map((obj, index) => (
+              <Guess
+                key={index}
+                issue={obj.issue}
+                releaseYear={obj.releaseYear}
+              />
+            ))}
+          </div>
+          <footer id="guesses-footer">Arvaukset jäljellä: 5</footer>
+        </div>
       </div>
       <div id="form-container">
         <form action={onFormSubmit} id="guess-form">
@@ -19,15 +57,19 @@ function App() {
               <input
                 name="issue"
                 type="number"
+                inputMode="numeric"
                 className="guess-input"
                 placeholder="Painos"
+                required
               />
               <span className="space-span">-</span>
               <input
                 name="release_year"
                 type="number"
+                inputMode="numeric"
                 className="guess-input"
                 placeholder="Vuosi"
+                required
               />
               <div className="submit-button-container">
                 <input type="submit" className="submit-button" value="Submit" />
