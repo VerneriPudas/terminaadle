ced0d306d072151eaff718f9da3001892140fa02
VerneriPudas <verkku.pudas@gmail.com>
2025-09-22 22:15:23 +0300

    finish implementing json generator, create get_random.py

--- data/generate_dataset.py
+++ data/generate_dataset.py
@@ -1,15 +1,47 @@
 import json
 import os
 
-# Directory that contains pdf files
 source_dir = "pdf_downloads"
 destination_name = "dataset.json"
 
 
 def generate_dataset(source, destination_name):
-    for filename in os.listdir(source):
-        print(os.path.join(source, filename))  # this just prints the filename
-        # Actually do the json generation
+    files = []
+    # Files with the new index style terminaali<yy><ee>
+    edition_max = 10
+    year_max = 25
+    name = "terminaali"
+    format = ".pdf"
+
+    year = 2011
+    while year < year_max + 1:
+        edition = 0
+        while edition < edition_max + 1:
+            filename = name + str(year)[-2:] + str(edition).zfill(2) + format
+            if os.path.isfile(os.path.join(source, filename)):
+                files.append({"filename": filename, "year": year, "edition": edition})
+            edition += 1
+        year += 1
+
+    # Files with older style indexing terminaali<ee><yy>
+    # Edition numbers are not zeropadded in the older style
+    year = 1988
+    year_max = 2011
+    while year < year_max + 1:
+        edition = 0
+        while edition < edition_max + 1:
+            filename = name + str(edition) + str(year)[-2:] + format
+            if os.path.isfile(os.path.join(source, filename)):
+                files.append({"filename": filename, "year": year, "edition": edition})
+            edition += 1
+        year += 1
+
+    # Handling pecial editions goes here
+    # TODO
+
+    # JSONify
+    with open(destination_name, "w", encoding="utf-8") as f:
+        json.dump(files, f, indent=4)
 
 
 if __name__ == "__main__":
---
+++ data/get_random.py
@@ -0,0 +1,31 @@
+import json
+import random
+import os
+from pypdf import PdfReader
+from pdf2image import convert_from_path
+
+if __name__ == "__main__":
+    pdf_dir = "pdf_downloads"
+    json_file = "dataset.json"
+
+    with open(json_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    random_item = random.choice(data)
+    filename = random_item["filename"]
+    path_to_pdf = os.path.join(pdf_dir, filename)
+
+    reader = PdfReader(path_to_pdf)
+    num_pages = len(reader.pages)
+
+    random_page_num = random.randint(0, num_pages - 1)
+    print("Page " + str(random_page_num + 1) + " from " + filename)
+
+    images = convert_from_path(
+        path_to_pdf, first_page=random_page_num + 1, last_page=random_page_num + 1
+    )
+    output_file = (
+        f"{random_item['year']}{random_item['edition']}_page{random_page_num + 1}.jpg"
+    )
+    images[0].save(output_file, "JPEG")
+    print("Saved: ", output_file)
