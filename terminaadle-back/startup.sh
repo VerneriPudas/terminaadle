abcb642eea52377b195466c758f0a5b96da50c45
Santeri Hyvärinen <santeri.hyvarinen@icloud.com>
2025-09-26 21:29:12 +0300

    Shit is fucked

--- terminaadle-back/startup.sh
+++ terminaadle-back/startup.sh
@@ -1,6 +1,5 @@
 cd data/
-./download_files.sh https://otit.fi/toiminta/terminaali/verkkoversiot/ &&\
-python generate_dataset.py
+./download_files.sh https://otit.fi/toiminta/terminaali/verkkoversiot/ && python generate_dataset.py
 
 cd ..
-exec uvicorn main:app --host 0.0.0.0 --port 8080
+exec python -m uvicorn main:app --host 0.0.0.0 --port 8080
