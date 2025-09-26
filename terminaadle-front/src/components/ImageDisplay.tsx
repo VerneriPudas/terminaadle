4815540beaf3d5a4fda10d71e96543a315284ff1
Santeri Hyvärinen <santeri.hyvarinen@icloud.com>
2025-09-26 19:00:48 +0300

    Fix bug related to imageDisplay not working properly when server fails to deliver base64 encoded image.

--- terminaadle-front/src/components/ImageDisplay.tsx
+++ terminaadle-front/src/components/ImageDisplay.tsx
@@ -2,7 +2,9 @@ interface ImageDisplayProps {
   base64Image?: string;
 }
 export default function ImageDisplay({ base64Image }: ImageDisplayProps) {
-  if (!base64Image) return;
+  if (!base64Image) {
+    return <div id="terminaali-page">Kuvaa ei löytynyt</div>;
+  }
 
   const imageUrl = `data:image/jpeg;base64,${base64Image}`;
   return <img id="terminaali-page" src={imageUrl} />;
